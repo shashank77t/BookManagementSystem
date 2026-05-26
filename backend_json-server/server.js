fd98e64241f61f076774bee6aa6726a55d0b947d
Shashank Yemjala <shashank.yemjal@gmail.com>
2026-05-26 18:13:29 +0530

    backend

---
+++ backend_json-server/server.js
@@ -0,0 +1,19 @@
+const jsonServer = require('json-server');
+
+const server = jsonServer.create();
+
+const router = jsonServer.router('db.json');
+
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+
+server.use(jsonServer.bodyParser);
+
+server.use(router);
+
+const PORT = process.env.PORT || 10000;
+
+server.listen(PORT, () => {
+  console.log(`JSON Server running on ${PORT}`);
+});
